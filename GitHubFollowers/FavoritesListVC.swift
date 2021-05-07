8d74bf89f7c08ea6da3c7099322c01f99efa5986
gokberkkoksoy <gokberkkoksoy@icloud.com>
2021-05-07 16:08:37 +0300

    App Navigation Setup

--- GitHubFollowers/FavoritesListVC.swift
+++ GitHubFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GitHubFollowers
 //
 //  Created by Gökberk Köksoy on 7.05.2021.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Gökberk Köksoy on 7.05.2021.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
