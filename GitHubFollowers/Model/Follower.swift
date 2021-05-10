64600ff7276b414cb5b099c009435850037a64b5
gokberkkoksoy <gokberkkoksoy@icloud.com>
2021-05-10 20:03:16 +0300

    Create Codable Models and NavigationBar Bug Fix

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Gökberk Köksoy on 10.05.2021.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by Gökberk Köksoy on 10.05.2021.
+//
+
+import Foundation
+
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
