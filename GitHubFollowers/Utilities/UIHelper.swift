702f94a19acca75bbfedd4329746e9924980306a
gokberkkoksoy <gokberkkoksoy@icloud.com>
2021-05-13 17:42:11 +0300

    Refactoring and Memory Management

---
+++ GitHubFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,24 @@
+//
+//  UIHelper.swift
+//  GitHubFollowers
+//
+//  Created by Gökberk Köksoy on 13.05.2021.
+//
+
+import UIKit
+
+struct UIHelper {
+    static func getThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+}
