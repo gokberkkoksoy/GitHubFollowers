ddabc861c8340eeed6fb1687043bfeaf81c01fa7
gokberkkoksoy <gokberkkoksoy@icloud.com>
2021-05-17 02:08:05 +0300

    Implement Empty State View

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -55,6 +55,11 @@ class FollowerListVC: UIViewController {
             case.success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                if self.followers.isEmpty {
+                    let message = "This user does not have any followers. Go follow them😄."
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+                    return
+                }
                 self.updateData()
                 
             case.failure(let error):
