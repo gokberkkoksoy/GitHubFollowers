87a326143cba3a4f5dfee80d6cb1ad91c7848ce1
gokberkkoksoy <gokberkkoksoy@icloud.com>
2021-05-20 02:27:09 +0300

    Create GFFollowerItemVC(Subclass)

---
+++ GitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,21 @@
+//
+//  GFFollowerItemVC.swift
+//  GitHubFollowers
+//
+//  Created by Gökberk Köksoy on 20.05.2021.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoView1.set(itemInfoType: .followers, with: user.followers)
+        itemInfoView2.set(itemInfoType: .following, with: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
