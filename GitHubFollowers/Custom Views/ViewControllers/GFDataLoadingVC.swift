64d4ce3a07dc8658a74d852bed457b788d7d9b08
gokberkkoksoy <gokberkkoksoy@icloud.com>
2021-05-26 01:35:45 +0300

    GFDataLoadingVC is the superclass of VCs that loads data

---
+++ GitHubFollowers/Custom Views/ViewControllers/GFDataLoadingVC.swift
@@ -0,0 +1,45 @@
+//
+//  GFDataLoadingVC.swift
+//  GitHubFollowers
+//
+//  Created by Gökberk Köksoy on 26.05.2021.
+//
+
+import UIKit
+
+class GFDataLoadingVC: UIViewController {
+    
+    var containerView: UIView!
+
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0 // initially invisible
+        UIView.animate(withDuration: 0.25) { self.containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            self.containerView.removeFromSuperview()
+            self.containerView = nil
+        }
+    }
+    
+    func showEmptyStateView(with message: String, in view: UIView) {
+        let emptyStateView = GFEmptyStateView(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
+
+}
